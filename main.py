44bba09c1e54ea0584400e85766e4e2a4b2f1f35
anmol <anmol@localhost.localdomain>
2019-11-05 22:08:07 +0530

    Added Comment

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from captcha.image import ImageCaptcha
 
 class MathCaptcha:
     def __init__(self):
-        root = Tk()
+        root = Tk() #root Tkinter
         self.actual = str(random.randint(0, 9)) + \
             self.randOps()+str(random.randint(0, 9))
-        print(self.actual)
-        image = ImageCaptcha(fonts=['captcha.ttf'])
-        data = image.generate(self.actual)
-        image.write(self.actual, "out.png")
+        print(self.actual) #For generating random string of mathmatical expression 
+        image = ImageCaptcha(fonts=['captcha.ttf']) #Font for captcha
+        data = image.generate(self.actual) 
+        image.write(self.actual, "out.png") #Output image of captcha
         photo = PhotoImage(file=r"out.png")
-        Button(root, text="Submit", image=photo).pack(side=TOP)
+        Button(root, text="Submit", image=photo).pack(side=TOP) #Putting image of captcha in tkinter 
         Label(root, text="Enter the captcha:").pack(side=TOP)
         self.captchaInput = Entry(root)
         self.captchaInput.pack(side=TOP)
@@ -23,10 +23,10 @@ class MathCaptcha:
 
     def randOps(self):
         ops = ('+', '-', '*', '/')
-        return random.choice(ops)
+        return random.choice(ops) #For choosingrandom operator the list as String 
 
     def submit(self):
-        if(int(self.captchaInput.get()) == eval(self.actual)):
+        if(int(self.captchaInput.get()) == eval(self.actual)): #eval() converts returns the output of mathematical expression  
             print("Captcha Verified")
         else:
             print("Incorrect Captcha")
